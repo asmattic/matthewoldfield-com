2d00850893c909e77877b99b11010717440e4be0
Matt Oldfield <mattoldfieldweb@gmail.com>
2022-09-12 08:03:21 -0400

    task: added resume

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,7 +6,13 @@ const projectsData = [
     href: '/blog/gasparilla-music-festival/gasparilla-music-festival-2021',
   },
   {
-    title: 'React Analytics Dashboard',
+    title: '',
+    description: `React Analytics Dashboard (2021)`,
+    imgSrc: '/static/images/rf-dashboard-home.png',
+    href: '/blog/react-analytics-dashboard',
+  },
+  {
+    title: 'React Analytics Dashboard (2018)',
     description: `React Analytics Dashboard`,
     imgSrc: '/static/images/dashboard-report-aug-2017.jpg',
     href: '/blog/react-analytics-dashboard-report',
@@ -17,6 +23,8 @@ const projectsData = [
     imgSrc: '/static/images/my-shower-door-full-02-2018.jpg',
     href: '/blog/my-shower-door',
   },
+
+
 ]
 
 export default projectsData
