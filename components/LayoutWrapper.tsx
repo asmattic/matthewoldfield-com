fabe6686fe7cdc79608b3864369bb42a720ef839
Matt Oldfield <mattoldfieldweb@gmail.com>
2022-09-12 06:31:14 -0400

    task: added gmf, msd, analytics dashboard

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,18 +1,17 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-// import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
-import {ReactNode} from 'react'
+import { ReactNode } from 'react'
 import Image from './Image'
 interface Props {
   children: ReactNode
 }
 
-const LayoutWrapper = ({children}: Props) => {
+const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Gasparilla Music Festival',
+    description: `Gasparilla Music Festival`,
+    imgSrc: '/static/images/gasparilla-music-festival-full-2016.jpg',
+    href: '/blog/gasparilla-music-festival',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'React Analytics Dashboard',
+    description: `React Analytics Dashboard`,
+    imgSrc: '/static/images/dashboard-report-aug-2017.jpg',
+    href: '/blog/react-analytics-dashboard-report',
   },
 ]
 
