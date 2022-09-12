8c000c554122fed8364f8a05bbcbd638fcdc4dcf
Matt Oldfield <mattoldfieldweb@gmail.com>
2022-09-12 00:32:48 -0400

    cleanup

--- components/analytics/index.tsx
+++ components/analytics/index.tsx
@@ -1,8 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import GA from './GoogleAnalytics'
-import Plausible from './Plausible'
-import SimpleAnalytics from './SimpleAnalytics'
-import Umami from './Umami'
 import siteMetadata from '@/data/siteMetadata'
 
 declare global {
@@ -16,18 +13,7 @@ declare global {
 const isProduction = process.env.NODE_ENV === 'production'
 
 const Analytics = () => {
-  return (
-    <>
-      {isProduction && siteMetadata.analytics.plausibleDataDomain && (
-        <Plausible />
-      )}
-      {isProduction && siteMetadata.analytics.simpleAnalytics && (
-        <SimpleAnalytics />
-      )}
-      {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
-      {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
-    </>
-  )
+  return <>{isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}</>
 }
 
 export default Analytics
--- components/comments/index.tsx
+++ components/comments/index.tsx
@@ -7,18 +7,6 @@ interface Props {
   frontMatter: CoreContent<Blog>
 }
 
-const UtterancesComponent = dynamic(
-  () => {
-    return import('@/components/comments/Utterances')
-  },
-  {ssr: false},
-)
-const GiscusComponent = dynamic(
-  () => {
-    return import('@/components/comments/Giscus')
-  },
-  {ssr: false},
-)
 const DisqusComponent = dynamic(
   () => {
     return import('@/components/comments/Disqus')
@@ -31,13 +19,6 @@ const Comments = ({frontMatter}: Props) => {
   if (!comment || Object.keys(comment).length === 0) return <></>
   return (
     <div id="comment">
-      {siteMetadata.comment && siteMetadata.comment.provider === 'giscus' && (
-        <GiscusComponent />
-      )}
-      {siteMetadata.comment &&
-        siteMetadata.comment.provider === 'utterances' && (
-          <UtterancesComponent />
-        )}
       {siteMetadata.comment && siteMetadata.comment.provider === 'disqus' && (
         <DisqusComponent frontMatter={frontMatter} />
       )}
