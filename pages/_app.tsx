67cc2cf5be7cf29a71f4a8424be2adb1b39941f5
Matt Oldfield <matthewoldfieldweb@gmail.com>
2022-10-11 03:12:12 -0400

    Starting to add next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,22 @@ import '@fontsource/inter/variable-full.css'
 import {ThemeProvider} from 'next-themes'
 import type {AppProps} from 'next/app'
 import Head from 'next/head'
+import { SessionProvider } from "next-auth/react"
+import type { Session } from "next-auth"
 
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
-export default function App({Component, pageProps}: AppProps) {
+
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
   return (
+    <SessionProvider session={session}>
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
@@ -23,5 +32,6 @@ export default function App({Component, pageProps}: AppProps) {
         <Component {...pageProps} />
       </LayoutWrapper>
     </ThemeProvider>
+    </SessionProvider>
   )
 }
