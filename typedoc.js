f7fed8c6f35634495c6956710dd841baec82f8de
Matt Oldfield <mattoldfieldweb@gmail.com>
2023-02-28 05:49:02 +0000

    Add prettier vscode settings

--- typedoc.js
+++ typedoc.js
@@ -1,13 +1,17 @@
 module.exports = {
   name: '@asmattic/matthewoldfield-com',
   out: './docs/',
-  entryPoints: ["./tsconfig.json", "./components/**", "./pages/**", "./scripts/**", "./lib/**", "./layouts/**"],
-  exclude: [
-    './pages/server.tsx',
+  entryPoints: [
+    './tsconfig.json',
+    './components/**',
+    './pages/**',
+    './scripts/**',
+    './lib/**',
+    './layouts/**',
   ],
+  exclude: ['./pages/server.tsx'],
   excludeExternals: true,
   excludePrivate: true,
   hideGenerator: true,
-  readme: 'none'
-};
-
+  readme: 'none',
+}
